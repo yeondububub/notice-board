ab3cb33e37afe13a678b6ccb124aae650d735726
ydbb <yeondububub@gmail.com>
2026-07-01 11:08:16 +0900

    feat: 댓글 무한 depth - 테이블 및 인덱스 생성

--- service/comment/src/main/resources/db/rdb-schema.sql
+++ service/comment/src/main/resources/db/rdb-schema.sql
@@ -9,3 +9,15 @@ create table comment (
 );
 
 create index idx_article_id_parent_comment_id_comment_id on comment(article_id asc, parent_comment_id asc, comment_id asc);
+
+create table comment_v2 (
+    comment_id bigint not null primary key,
+    content varchar(3000) not null,
+    article_id bigint not null,
+    writer_id bigint not null,
+    path varchar(25) character set utf8mb4 collate utf8mb4_bin not null,
+    deleted bool not null,
+    created_at datetime not null
+);
+
+create unique index idx_article_id_path on comment_v2(article_id asc, path asc);
