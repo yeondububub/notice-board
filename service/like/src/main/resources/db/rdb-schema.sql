415b16de8d52ae642cc39e18111d05f2f7273199
ydbb <yeondububub@gmail.com>
2026-07-02 10:42:36 +0900

    feat: 좋아요 테이블 설계 및 인덱스 생성

---
+++ service/like/src/main/resources/db/rdb-schema.sql
@@ -0,0 +1,8 @@
+create table article_like (
+    article_like_id bigint not null primary key,
+    article_id bigint not null,
+    user_id bigint not null,
+    created_at datetime not null
+);
+
+create unique index idx_article_id_user_id on article_like(article_id asc, user_id asc);
