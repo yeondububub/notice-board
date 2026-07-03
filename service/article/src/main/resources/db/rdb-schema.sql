14cbd9803cf62b16e9c3439f45e9d84a756df111
ydbb <yeondububub@gmail.com>
2026-07-03 10:34:18 +0900

    feat: 게시글 수&댓글 수 DB설계

--- service/article/src/main/resources/db/rdb-schema.sql
+++ service/article/src/main/resources/db/rdb-schema.sql
@@ -1,9 +1,14 @@
 create table article (
-                         article_id bigint not null primary key,
-                         title varchar(100) not null,
-                         content varchar(3000) not null,
-                         board_id bigint not null,
-                         writer_id bigint not null,
-                         created_at datetime not null,
-                         modified_at datetime not null
+    article_id bigint not null primary key,
+    title varchar(100) not null,
+    content varchar(3000) not null,
+    board_id bigint not null,
+    writer_id bigint not null,
+    created_at datetime not null,
+    modified_at datetime not null
+);
+
+create table board_article_count (
+    board_id bigint not null primary key,
+    article_count bigint not null
 );
