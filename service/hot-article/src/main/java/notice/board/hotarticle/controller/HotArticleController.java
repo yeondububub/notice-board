535b7b35649d4a37939f556db3b276ed41f9e373
ydbb <yeondububub@gmail.com>
2026-07-09 15:06:42 +0900

    feat: 인기글 Consumer 구현 - 컨트롤러&컨슈머 작성

---
+++ service/hot-article/src/main/java/notice/board/hotarticle/controller/HotArticleController.java
@@ -0,0 +1,24 @@
+package notice.board.hotarticle.controller;
+
+import notice.board.hotarticle.service.HotArticleService;
+import notice.board.hotarticle.service.response.HotArticleResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class HotArticleController {
+
+    private final HotArticleService hotArticleService;
+
+    @GetMapping("/v1/hot-articles/articles/date/{dateStr}")
+    public List<HotArticleResponse> readAll(
+            @PathVariable("dateStr") String dateStr
+    ) {
+        return hotArticleService.readAll(dateStr);
+    }
+}
