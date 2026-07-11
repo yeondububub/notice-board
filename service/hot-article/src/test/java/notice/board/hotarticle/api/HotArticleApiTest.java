e2c29297ab465cdead577bd2f1d3a03448b2238b
ydbb <yeondububub@gmail.com>
2026-07-11 23:46:13 +0900

    test: 인기글 Producer&Consumer 테스트

---
+++ service/hot-article/src/test/java/notice/board/hotarticle/api/HotArticleApiTest.java
@@ -0,0 +1,25 @@
+package notice.board.hotarticle.api;
+
+import notice.board.hotarticle.service.response.HotArticleResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+public class HotArticleApiTest {
+    RestClient restClient = RestClient.create("http://localhost:9004");
+
+    @Test
+    void readAllTest() {
+        List<HotArticleResponse> responses = restClient.get()
+                .uri("/v1/hot-articles/articles/date/{dateStr}", 20260711)
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<HotArticleResponse>>() {
+                });
+
+        for (HotArticleResponse response : responses) {
+            System.out.println("response = " + response);
+        }
+    }
+}
