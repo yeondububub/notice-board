d2ac62d4da275eb235b2cd20f1cc796df2070be7
ydbb <yeondububub@gmail.com>
2026-07-12 12:54:51 +0900

    feat: CommentClient, LikeClient, ViewClient 생성

---
+++ service/article-read/src/main/java/notice/board/articleread/client/ViewClient.java
@@ -0,0 +1,36 @@
+package notice.board.articleread.client;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ViewClient {
+
+    private RestClient restClient;
+
+    @Value("${endpoints.notice-board-view-service.url}")
+    private String viewServiceUrl;
+
+    @PostConstruct
+    public void initRestClient(){
+        restClient = RestClient.create(viewServiceUrl);
+    }
+
+    public long count(Long articleId) {
+        try {
+            return restClient.get()
+                    .uri("/v1/article-views/articles/{articleId}/count", articleId)
+                    .retrieve()
+                    .body(Long.class);
+        } catch (Exception e) {
+            log.error("[ViewClient.count] articleId={}", articleId, e);
+            return 0;
+        }
+    }
+}
